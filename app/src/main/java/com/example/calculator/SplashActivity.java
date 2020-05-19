c4271070c4a60d507263f81b629a7cf855d2e59f
Nandkishor Kumawat <nandkishor.kumawat19@gmail.com>
2020-05-20 00:11:20 +0530

    first commit

---
+++ app/src/main/java/com/example/calculator/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.concurrent.Delayed;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+
+
+}
